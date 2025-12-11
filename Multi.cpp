db44b75bdc2da89e422a00745e21d521ef4172bd
code-ankisha <ankishamewal@gmail.com>
2025-12-11 20:50:32 +0530

    added multi.cpp

---
+++ Multi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, multi;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    multi = a + b;
+
+    cout <<"product is" << multi << endl;
+
+    return 0;
+}
